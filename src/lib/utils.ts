abb8aaa8876b511376be5522cb53f9d165e862b7
Xoai <xoajxanh.mu0j@gmail.com>
2026-07-10 01:33:50 +0700

    zxczxc

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,4 +5,4 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const API_BASE = "http://161.248.146.65:5229";
+export const API_BASE = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:5229";
